571befbcc59b2a6c6465166ab5c3c15d9b8cb40b
Manuel Quiroga <quirogamanuel21@gmail.com>
2024-03-03 22:07:22 -0600

    ajuste en estilos del formpayment

--- src/Components/Checkout/PaymentForm.js
+++ src/Components/Checkout/PaymentForm.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const PaymentForm = () => {
   return (
-    <div className="container mt-5 px-5">
+    <div className="container cpayment mt-5 px-5">
       <div className="mb-4">
         <h2>Confirm order and pay</h2>
         <span>
@@ -15,7 +15,7 @@ const PaymentForm = () => {
 
       <div className="row">
         <div className="col-md-8">
-          <div className="card p-3">
+          <div className="card ccard p-3">
             <h6 className="text-uppercase">Payment details</h6>
             <div className="inputbox mt-3">
               <input
@@ -121,32 +121,17 @@ const PaymentForm = () => {
               </div>
             </div>
           </div>
-
-          <div className="mt-4 mb-4 d-flex justify-content-between">
-            <span>Previous step</span>
-            <button className="btn btn-success px-3">Pay $840</button>
-          </div>
         </div>
 
         <div className="col-md-4">
           <div className="card card-blue p-3 text-white mb-3">
-            <span>You have to pay</span>
-            <div className="d-flex flex-row align-items-end mb-3">
-              <h1 className="mb-0 yellow">$549</h1>
-              <span>.99</span>
-            </div>
-            <span>
-              Enjoy all the features and perk after you complete the payment
-            </span>
-            <a href="/" className="yellow decoration">
-              Know all the features
-            </a>
-
-            <div className="hightlight">
-              <span>
-                100% Guaranteed support and update for the next 5 years.
-              </span>
-            </div>
+            
+          </div>
+          <div className="mt-4 mb-4 d-flex">
+            <span>Previous step</span>
+          </div>
+          <div className="mt-4 mb-4 d-flex justify-content-between">
+            <button className="btn btn-success btnOK px-3">Pay $840</button>
           </div>
         </div>
       </div>
